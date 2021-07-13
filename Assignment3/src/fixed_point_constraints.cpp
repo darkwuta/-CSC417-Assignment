ba83e01283b6418a641882b7438e1cfc8accdd92
darkwuta <z214971500@hotmail.com>
2021-07-13 23:01:22 +0800

    完成动能的计算，下面进行势能的计算，matlab可以求导

--- Assignment3/src/fixed_point_constraints.cpp
+++ Assignment3/src/fixed_point_constraints.cpp
@@ -1,7 +1,12 @@
 #include <fixed_point_constraints.h>
 #include <algorithm>
 void fixed_point_constraints(Eigen::SparseMatrixd &P, unsigned int q_size, const std::vector<unsigned int> indices) {
-
-    
+    P.resize(q_size - indices.size() * 3, q_size);
+    P.setZero();
+    int count = 0;
+    for (int i = 0; i < q_size - 3 * indices.size(); i++) {
+        if (count < indices.size() && i == 3.0 * indices[count] - 3.0 * count) { count++; }
+        P.insert(i, i + 3.0 * count) = 1.0;
+    }
 
 }
